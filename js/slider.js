84c5d91660162e4857ed89529cf16ee92b96e967
henkrs1 <eahenke@gmail.com>
2015-09-24 21:39:05 -0500

    added support for multiple sliders

--- js/slider.js
+++ js/slider.js
@@ -1,29 +1,55 @@
 (function() {
 	"use strict";
 
-	var timerId;
-
 	$(document).ready(function() {
-		assignSlideClasses();
-		activateControls();		
-		beginSlider();
+		
+		//create new Slider object for each '.slider-wrapper' found, and initialize.
+		//sliders as objects allows for multiple indepedent sliders on the same page.
+		//to style differently (size, margins, etc), add specific classes in SCSS file.
+		$('.slider-wrapper').each(function() {			
+			var slider = new Slider($(this));
+			slider.assignSlideClasses();
+			slider.activateControls();		
+			slider.beginSlider();
+		});
 	});
 
-	function beginSlider() {
-			timerId = setTimeout(function() {
-			nextSlide(true);
-			}, 5000);		
+
+	//Slider object, holds info on slider specific node, timer, and controls
+	function Slider(node) {
+		this.context = node;
+		this.timerId;
+
+		this.arrowRight = this.context.find('.arrow-right');
+		this.arrowLeft = this.context.find('.arrow-left');
+		this.dots = this.context.find('.dot');
+	}
+	window.Slider = Slider;
+
+	Slider.prototype = {
+		constructor: Slider
+	};
+			
+	//Starts the slider timer
+	Slider.prototype.beginSlider = function() {
+		var self = this;
+		self.timerId = setTimeout(function() {
+		self.nextSlide(true);
+		}, 5000);		
 	}	
 
+	//Moves slides and restarts the slider timer.
 	//if passed true, slides forwards, if false, slides backwards
-	function nextSlide(forward) {
-		clearTimeout(timerId); //reset Slider timer;
+	Slider.prototype.nextSlide = function(forward, context) {
+		var self = this;
 
-		deactivateControls();
+		clearTimeout(this.timerId); //reset Slider timer;
+
+		this.deactivateControls();
 
 		var leftOrRight;
 		var slidesToMove;
-		var slideWidth = $('.slide').width();
+		var slideWidth = $('.slide', context).width();
 
 		if(forward) {
 			leftOrRight = "-=";
@@ -33,19 +59,20 @@
 			slidesToMove = '.slide.active, .slide.left';
 		}
 
-		$(slidesToMove).animate({
+		self.context.find(slidesToMove).animate({
 				left: leftOrRight + slideWidth,
 			}, 1000
 			).promise().done(function(){
-				queueSlides(forward);
-				assignSlideClasses();
-				activateControls();
-				beginSlider();
+				self.queueSlides(forward);
+				self.assignSlideClasses();
+				self.activateControls();
+				self.beginSlider();
 			});					
 	}
 
-	function assignSlideClasses() {
-		var current = $('.slide.active');
+	//Sets up classes for current slide, next and previous slides
+	Slider.prototype.assignSlideClasses = function() {
+		var current = this.context.find('.slide.active');
 		var prev;
 		var next;
 
@@ -65,61 +92,65 @@
 
 		prev.addClass('left');
 		next.addClass('right');		
-		dotMatch(current);
+		this.dotMatch(current);
 	}
 
 	//After movement, based on direction, determines new active slide and removes old classes 
-	function queueSlides(forward) {
+	Slider.prototype.queueSlides = function(forward) {
 		
 		//must come before assigning the new active slide or both slides are stripped.
-		$('.slide.active').removeClass('active');
+		this.context.find('.slide.active').removeClass('active');
 
 		if(forward) {
-			$('.slide.right').removeClass('right').addClass('active');
+			this.context.find('.slide.right').removeClass('right').addClass('active');
 		} else {
-			$('.slide.left').removeClass('left').addClass('active');
+			this.context.find('.slide.left').removeClass('left').addClass('active');
 		}
 
 		//strip left and right classes from all slides, leave only active
-		$('.slide').removeClass('right left').removeAttr('style');
+		this.context.find('.slide').removeClass('right left').removeAttr('style');
 	}
 
 	//Matches up the dot with the current slide.
-	function dotMatch(currentSlide) {
+	Slider.prototype.dotMatch = function(currentSlide) {
 		var index = currentSlide.index();
-		$('.dot').removeClass('active');
-		$('.dot').eq(index).addClass('active');	
+		this.context.find('.dot').removeClass('active');
+		this.context.find('.dot').eq(index).addClass('active');	
 
 	}
 
 	//Based on which dot is clicked, sets up the corresponding slide (based on index) to be the next slide.
-	function goToSlide() {
-		var dotIndex = $(this).index();
-		var currentIndex = $('.dot.active').index();
+	Slider.prototype.goToSlide = function(dot) {
+		var dotIndex = dot.index();
+		var currentIndex = dot.parent().find('.active').index();
 
-		//disables current dot from being selected.
-		if(dotIndex != currentIndex) {		
+		console.log('clicked ' + dotIndex);
+		console.log('current ' + currentIndex);
 
-			$('.slide').removeClass('left right');
+		//disallows current dot from being selected
+		if(dotIndex != currentIndex) {
+			dot.parents('.slider-wrapper').find('.slide').removeClass('left right');
 
-			var forward;
-			var directionClass;
+			var forward, directionClass;
 
-			if(dotIndex > currentIndex) {
+			if(currentIndex < dotIndex) {
 				directionClass = 'right';
 				forward = true;
 			} else {
 				directionClass = 'left';
 				forward = false;
 			}
-			
-			$('.slide').eq(dotIndex).addClass(directionClass);			
-			nextSlide(forward);
+
+			dot.parents('.slider-wrapper').find('.slide').eq(dotIndex).addClass(directionClass);
+			this.nextSlide(forward);
 		}
 	}
 
 	//Turns on Slider controls.  Should be turned on initially, and after animation has completed.
-	function activateControls() {
+	Slider.prototype.activateControls = function() {
+		var self = this;
+		
+
 		$('.arrow').mousedown(function() {
 			$(this).find('span').addClass('clicked');
 		});
@@ -128,20 +159,24 @@
 			$(this).find('span').removeClass('clicked');
 		});
 
-		$('.arrow-right').click(function(){
-			nextSlide(true);
-		});
-		$('.arrow-left').click(function() {
-			nextSlide(false);
+		self.arrowRight.click(function(){
+			self.nextSlide(true);
 		});
 
-		$('.dot').click(goToSlide);	
+		self.arrowLeft.click(function() {			
+			self.nextSlide(false);
+		});
+
+		self.dots.click(function() {
+			self.goToSlide($(this));			
+		});
+			
 	}
 
 	//Turns off Slider controls.  Should be called during animation to prevent click spamming.
-	function deactivateControls() {
-		$('.dot').off();
-		$('.arrow').off();
+	Slider.prototype.deactivateControls = function() {
+		this.context.find('.dot').off();
+		this.context.find('.arrow').off();
 			
 	}	
 		
